25aa8f503583942769dddb9c20fdd5eb33b39c7f
Kritika <kritsme963@gmail.com>
2022-11-23 10:19:03 -0800

    Added:Tree program

---
+++ src/com/tree/BinaryTree.java
@@ -0,0 +1,12 @@
+package com.tree;
+
+public class BinaryTree {
+    public static void main(String[] args) {
+        MyBinaryTree<Integer> mybinarytree = new MyBinaryTree<>();
+        mybinarytree.add(56);
+        mybinarytree.add(30);
+        mybinarytree.add(70);
+    }
+
+
+}
