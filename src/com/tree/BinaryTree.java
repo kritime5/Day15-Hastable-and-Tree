8dd1a02b9fbcc33b16532b5dde9ae1f6b8572cbd
Kritika <kritsme963@gmail.com>
2022-11-23 10:28:11 -0800

    Added:add 56 as root and then 30 and 70

--- src/com/tree/BinaryTree.java
+++ src/com/tree/BinaryTree.java
@@ -3,6 +3,7 @@ package com.tree;
 public class BinaryTree {
     public static void main(String[] args) {
         MyBinaryTree<Integer> mybinarytree = new MyBinaryTree<>();
+        //UC1-CREATE BST BY ADDING 56 AND THEN 30 AND 70
         mybinarytree.add(56);
         mybinarytree.add(30);
         mybinarytree.add(70);
